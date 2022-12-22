9187d74201eab799bda676cc1fb62a82e176e681
Diana Nekrasova <ya.nel2014@ya.ru>
2022-12-22 12:53:09 +0300

    Added inheritance

--- src/main/java/org/lab9/entity/Balance.java
+++ src/main/java/org/lab9/entity/Balance.java
@@ -3,18 +3,14 @@ package org.lab9.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-public class Balance {
-    @Getter @Setter private Integer id;
+public class Balance extends Account{
     @Getter @Setter private Integer incomeCategoryId;
-    @Getter @Setter private Double currentSum;
-    @Getter @Setter private String currency;
-    @Getter @Setter private String name;
     @Getter @Setter private Double percent;
 
     public void addMoney(Double sum){
-        this.currentSum += sum;
+        this.setCurrentSum(this.getCurrentSum() + sum);
     }
     public void removeMoney(Double sum){
-        this.currentSum -= sum;
+        this.setCurrentSum(this.getCurrentSum() - sum);
     }
 }
--- src/main/java/org/lab9/entity/Goal.java
+++ src/main/java/org/lab9/entity/Goal.java
@@ -3,18 +3,15 @@ package org.lab9.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-public class Goal {
-    @Getter @Setter private Integer id;
-    @Getter @Setter private String name;
+public class Goal extends Account{
     @Getter @Setter private Integer duration;
-    @Getter @Setter private Double currentSum;
     @Getter @Setter private Double wishSum;
 
     public void addMoney(Double money) {
-        this.currentSum = this.currentSum + money;
+        this.setCurrentSum(this.getCurrentSum() + money);
     }
 
     public Double getRequired() {
-        return this.wishSum - this.currentSum;
+        return this.wishSum - this.getCurrentSum();
     }
 }
