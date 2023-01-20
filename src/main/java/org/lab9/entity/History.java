80f57811c9aa33773bf5add565b64d52ccac9c88
Diana Nekrasova <ya.nel2014@ya.ru>
2023-01-20 20:12:50 +0300

    Added interface

--- src/main/java/org/lab9/entity/History.java
+++ src/main/java/org/lab9/entity/History.java
@@ -1,15 +1,9 @@
 package org.lab9.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+public interface History<T> {
 
-import java.util.List;
-
-public abstract class History<T> {
-    @Getter @Setter private List<T> operationsList;
-
-    public abstract void addOperation(T operation);
-    public abstract T findOperationById(Integer id);
-    public abstract void removeOperationById(Integer id);
-    public abstract String toString();
+    void addOperation(T operation);
+     T findOperationById(Integer id);
+    void removeOperationById(Integer id);
+    String toString();
 }
--- src/main/java/org/lab9/entity/TransactionHistory.java
+++ src/main/java/org/lab9/entity/TransactionHistory.java
@@ -3,17 +3,18 @@ package org.lab9.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class TransactionHistory extends History <Transaction>{
+public class TransactionHistory implements History<Transaction> {
     @Getter @Setter private List<Transaction> transactions;
     @Override
     public void addOperation(Transaction transaction) {
         this.transactions.add(transaction);
     }
+
+
     public Transaction findOperationById(Integer id) {
         for (Transaction transaction : this.transactions) {
             if (transaction.getId().equals(id)) {
@@ -30,7 +31,7 @@ public class TransactionHistory extends History <Transaction>{
     @Override
     public String toString() {
         Stream<Transaction> st = this.transactions.stream();
-        String result = st.map(Transaction::toString).collect(Collectors.joining("\n"));;
+        String result = st.map(Transaction::toString).collect(Collectors.joining("\n"));
         return result;
     }
 }
